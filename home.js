00fd0639dc52fd762b387d741d0f30e5f568de63
usman-52 <usman914.764@gmail.com>
2024-03-25 19:33:03 +0000

    i add javaScript file

--- home.js
+++ home.js
@@ -1,8 +1,14 @@
-function togglePlayPause() {
-    var myAudio = document.getElementById("myAudio");
-    if (myAudio.paused) {
-        myAudio.play();
-    } else {
-        myAudio.pause();
-    }
+// ! This is javaScript code for audio input
+// var audio = new Audio();
+
+/*function playAudio() {
+    audio.play();
+}
+
+document.getElementById("audio-player").addEventListener("click",playAudio);
+*/
+
+function playAudio(){
+    var audio = document.getElementById("mySound");
+    audio.play();
 }
